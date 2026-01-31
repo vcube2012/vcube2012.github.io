9b28bff12ef5462782ecb12a924b2c523e1a0a0e
vcube2012 <vcube.su@gmail.com>
2026-01-31 14:41:21 +0200

    Change title and credits from Professor6T9 to AnonNexus

--- admin.php
+++ admin.php
@@ -250,7 +250,7 @@ if ($content === FALSE) {
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
-  <title>Professor6T9 Web SheLL</title>
+  <title>AnonNexus Web SheLL</title>
 </head>
 
 <body class="bg-dark text-light">
@@ -457,7 +457,7 @@ if ($content === FALSE) {
             </tbody>
           </table>
         </div>
-        <div class="text-light">&#169; Professor6T9 <script type='text/javascript'>var creditsyear = new Date();document.write(creditsyear.getFullYear());</script></div>
+        <div class="text-light">&#169; AnonNexus <script type='text/javascript'>var creditsyear = new Date();document.write(creditsyear.getFullYear());</script></div>
       </div>
     </div>
   </div>
